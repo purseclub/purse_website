18b5398a8c3349c46f044baba528d2b3312d9f37
Abhishekucs <abhishekucskumar@gmail.com>
2021-10-10 03:48:30 +0530

    loading animation added v1

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,16 +1,104 @@
-import React from "react";
-import {
-  LoaderWrapper,
-  Percentage,
-  PercentageIndicator,
-} from "./styles/StyledLoader";
+import { useAnimation } from "framer-motion";
+import React, { useEffect } from "react";
+import { LoaderWrapper, Typo, TypoContainer } from "./styles/StyledLoader";
+
+const containerVariant = {
+  animate: {
+    transition: {
+      staggerChildren: 1,
+    },
+  },
+};
+
+const typoVariants = {
+  animate: {
+    x: ["20%", "-200%"],
+    opacity: [1, 0],
+    transition: {
+      duration: 2.3,
+      ease: "easeOut",
+    },
+  },
+};
 
 const Loader = () => {
+  const containerControl = useAnimation();
+  const typoControl = useAnimation();
+
+  useEffect(() => {
+    const sequence = async () => {
+      await containerControl
+        .start({
+          scale: 2.7,
+          rotate: "-90deg",
+          transition: {
+            duration: 1.4,
+            ease: "easeInOut",
+          },
+        })
+        .then(
+          await typoControl.start({
+            x: "20%",
+            opacity: 1,
+            transition: {
+              duration: 1,
+              ease: "easeInOut",
+            },
+          })
+        );
+      //   typoControl.set({
+      //     x: "20%",
+      //     opacity: 1,
+      //     transition: {
+      //       staggerChildren: 0.04,
+      //     },
+      //   });
+
+      //   await typoControl.start({
+      //     x: "20%",
+      //     transition: {
+      //       duration: 1,
+      //       ease: "easeInOut",
+      //     },
+      //   });
+      await typoControl.start({
+        x: "-200%",
+        opacity: 0,
+        transition: {
+          duration: 1.5,
+          ease: "easeIn",
+        },
+      });
+      //   await typoControl.start({
+      //     opacity: 1,
+      //     transition: {
+      //       duration: 1,
+      //       ease: "easeIn",
+      //     },
+      //   });
+      //   await typoControl.start({
+      //     opacity: 0,
+      //     transition: {
+      //       duration: 1.5,
+      //       ease: "easeIn",
+      //     },
+      //   });
+    };
+
+    sequence();
+  }, [containerControl, typoControl]);
   return (
-    <LoaderWrapper id="preloader">
-      <PercentageIndicator>
-        <Percentage>80</Percentage>
-      </PercentageIndicator>
+    <LoaderWrapper id="preloader" aria-hidden="true">
+      <TypoContainer animate={containerControl} variants={containerVariant}>
+        <Typo animate={typoControl}>bonjour bonjour bonjour</Typo>
+        <Typo animate={typoControl}>bonjour bonjour bonjour</Typo>
+        <Typo animate={typoControl}>bonjour bonjour bonjour</Typo>
+        <Typo animate={typoControl}>bonjour bonjour bonjour</Typo>
+        <Typo animate={typoControl}>bonjour bonjour bonjour</Typo>
+        <Typo animate={typoControl}>bonjour bonjour bonjour</Typo>
+        <Typo animate={typoControl}>bonjour bonjour bonjour</Typo>
+        <Typo animate={typoControl}>bonjour bonjour bonjour</Typo>
+      </TypoContainer>
     </LoaderWrapper>
   );
 };
--- src/components/loader/styles/StyledLoader.js
+++ src/components/loader/styles/StyledLoader.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const LoaderWrapper = styled.div`
@@ -14,16 +15,34 @@ export const LoaderWrapper = styled.div`
   background-color: var(--black);
 `;
 
-export const PercentageIndicator = styled.div`
-  width: 5vw;
-  height: 5vw;
-  background-color: red;
+export const TypoContainer = styled(motion.div)`
+  position: fixed;
+  height: 100vmax;
+  text-transform: uppercase;
+  width: 100vmax;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+  top: 50%;
+  margin-top: -50vmax;
+  will-change: transform;
 `;
 
-export const Percentage = styled.h1`
-  font-family: var(--font-family-main);
-  font-size: 7vw;
-  line-height: 1.1;
-  letter-spacing: -0.01em;
+export const Typo = styled(motion.div)`
+  white-space: nowrap;
+  font-size: 12vh;
+  font-size: clamp(7rem, 18.75vh, 15rem);
+  line-height: 0.75;
+  font-weight: bold;
+  font-family: var(--font-family-main-SemiBold);
+  font-weight: 600;
   color: var(--white);
+  opacity: 0.1;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  will-change: transform, opacity;
 `;
